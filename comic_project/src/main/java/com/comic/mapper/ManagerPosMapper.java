d4bfb14c0616375331a164ccb2f92d5afea6bc81
dudgkr <tmehfld92@gmail.com>
2019-09-26 17:48:13 +0900

    190926
    17:48work

--- comic_project/src/main/java/com/comic/mapper/ManagerPosMapper.java
+++ comic_project/src/main/java/com/comic/mapper/ManagerPosMapper.java
@@ -1,5 +1,11 @@
 package com.comic.mapper;
 
+import org.apache.ibatis.annotations.Param;
+
 public interface ManagerPosMapper {
+	
+	public int room_start_insert(@Param("user") String user, @Param("sysdate") int sysdate,
+			@Param("user_status") String user_status,@Param("order_status") String order_status,
+			@Param("room_num") int room_num);
 
 }
